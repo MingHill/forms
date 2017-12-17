a3f81dd6fbaed7a72b03f0ce37e79918ca1e7208
JasonLev <jasond.levy@gmail.com>
2017-12-17 13:28:09 -0800

    starter code of html, css, and js

--- form.js
+++ form.js
@@ -1,5 +1,5 @@
 let inputs = document.querySelectorAll("input");
-inputs.forEach(el => el.addEventListener("click", labelInput));
+inputs.forEach(el => el.addEventListener("focus", labelInput));
 
 function labelInput(event) {
   console.log(event.target.id);
