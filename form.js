ac837fe920fb9cca0e9b527cddfc6b76964f3619
JasonLev <jasond.levy@gmail.com>
2017-12-17 15:04:59 -0800

    add placeholder dataset fixes

--- form.js
+++ form.js
@@ -1,6 +1,14 @@
 let inputs = document.querySelectorAll("input");
 inputs.forEach(el => el.addEventListener("focus", labelInput));
+inputs.forEach(el => el.addEventListener("blur", blurInput));
 
 function labelInput(event) {
-  console.log(event.target.id);
+  event.target.previousSibling.previousSibling.classList.add("showLabel");
+  event.target.attributes.placeholder.value = "";
+}
+function blurInput(event) {
+  event.target.attributes.placeholder.value = event.target.dataset.placeholder;
+  if (!event.target.value) {
+    event.target.previousSibling.previousSibling.classList.remove("showLabel");
+  }
 }
